d4c4b57f628a74fd6f30c128be91a6828f161245
naor zruk <naor.z@broadsay.com>
2019-01-20 19:22:13 +0200

    add infinite scrolling

--- src/Screens/MainScreen.js
+++ src/Screens/MainScreen.js
@@ -2,10 +2,8 @@ import React, { Component } from "react";
 import {
   ActivityIndicator,
   FlatList,
-  Image,
+  Modal,
   StyleSheet,
-  Text,
-  TouchableOpacity,
   View
 } from "react-native";
 import Header from "../Components/Header";
@@ -15,28 +13,62 @@ import ShowView from "../Components/ShowView";
 
 // Todo: Inspect the react-navigator header support
 
-export function dimensionPercToPixel(
-  perc: number,
-  dimension: "width" | "height"
-): number {
-  const screenDimension = Dimensions.get("window")[dimension];
-  return (screenDimension * perc) / 100;
-}
-
 export default class MainScreen extends Component {
   constructor(props) {
     super(props);
     this.state = { data: [], page: 0, isFetching: false };
   }
   async componentDidMount() {
-    console.log("did mount");
+    // Todo: handle a case of failure, consider adding lastFetchStatus state
+    this.fetchShowItems();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header title={"TV-MAZE"} />
+        <View style={styles.content}>
+          <Modal visible={this.state.isFetching} transparent={true}>
+            <View
+              style={{
+                display: "flex",
+                flex: 1,
+                justifyContent: "center",
+                alignContent: "center"
+              }}
+            >
+              <ActivityIndicator size="large" color="white" />
+            </View>
+          </Modal>
+          <FlatList
+            data={this.state.data}
+            renderItem={({ item }) => (
+              <ShowView
+                onPress={this.onShowPress(item)}
+                rating={item.rating}
+                image={item.image}
+                id={item.id}
+                name={item.name}
+                summary={item.summary}
+              />
+            )}
+            keyExtractor={({ id }) => id + ""}
+            onEndReached={this.fetchShowItems}
+          />
+        </View>
+        <Footer />
+      </View>
+    );
+  }
+
+  fetchShowItems = async () => {
     this.setState({ isFetching: true });
-    const res = await fetch("http://api.tvmaze.com/shows?page=0");
-    console.log("res", res);
+    const res = await fetch(
+      `http://api.tvmaze.com/shows?page=${this.state.page}`
+    );
     if (res.ok) {
       const curPag = this.state.page;
       const body = await res.json();
-      console.log("body", body[0]);
       this.setState({
         isFetching: false,
         data: this.state.data.concat(body),
@@ -46,45 +78,7 @@ export default class MainScreen extends Component {
     this.setState({
       isFetching: false
     });
-  }
-
-  render() {
-    const initFetch = this.state.data.length === 0 && this.state.isFetching;
-    // if (this.state.data.length === 0 && this.state.isFetching) {
-    //   return (
-    //     <View
-    //       style={{ flex: 1, justifyContent: "center", alignContent: "center" }}
-    //     >
-    //     </View>
-    //   );
-    // }
-    return (
-      <View style={styles.container}>
-        <Header title={"TV-MAZE"} />
-        <View style={styles.content}>
-          {initFetch ? (
-            <ActivityIndicator size="large" color="white" />
-          ) : (
-            <FlatList
-              data={this.state.data}
-              renderItem={({ item }) => (
-                <ShowView
-                  onPress={this.onShowPress(item)}
-                  rating={item.rating}
-                  image={item.image}
-                  id={item.id}
-                  name={item.name}
-                  summary={item.summary}
-                />
-              )}
-              keyExtractor={({ id }) => id + ""}
-            />
-          )}
-        </View>
-        <Footer />
-      </View>
-    );
-  }
+  };
 
   onShowPress = item => {
     const { id, name, summary, image } = item;
