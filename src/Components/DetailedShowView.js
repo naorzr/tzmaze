8e178442a30d03c668d8617ffe09e6c6fbf12831
naor zruk <naor.z@broadsay.com>
2019-01-21 07:45:46 +0200

    add detailedshow view comp

---
+++ src/Components/DetailedShowView.js
@@ -0,0 +1,63 @@
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  Text,
+  View,
+  StyleSheet
+} from "react-native";
+import React from "react";
+import HTML from "react-native-render-html";
+
+export default props => {
+  const { summary, image, rating, schedule, genres, network, language } = props;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.posterContainer}>
+        <Image
+          source={{ uri: image }}
+          style={{ width: "100%", height: "100%" }}
+          resizeMode="cover"
+        />
+      </View>
+      <View style={styles.detailsContainer}>
+        <Text style={styles.text}>{"Avg Rating  " + rating.average / 10}</Text>
+        <View>
+          <ScrollView style={styles.scrollView}>
+            <Text style={styles.text}>
+              {`Genres: ${genres}\n`}
+              {`Network: ${network.name}\n`}
+              {`Language: ${language}\n`}
+              {`${schedule.days} ${schedule.time}\n`}
+            </Text>
+            <HTML
+              html={summary}
+              imagesMaxWidth={Dimensions.get("window").width}
+            />
+          </ScrollView>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { display: "flex", flexDirection: "column", flex: 1 },
+  scrollView: { height: 140, marginTop: 20, backgroundColor: "teal" },
+  posterContainer: { flex: 1.5, backgroundColor: "green" },
+  detailsContainer: {
+    display: "flex",
+    flex: 1,
+    backgroundColor: "red",
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    flexDirection: "column"
+  },
+  text: {
+    backgroundColor: "teal",
+    width: "100%",
+    textAlign: "center",
+    color: "white"
+  }
+});
